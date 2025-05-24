ef672cd07f4293ee60c6fe7414932711b030337b
yousefHalsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2025-05-24 19:26:54 +0300

    trying character portrait idea
    the idea is I will look for the character portrait and from it I am able to know which character I am and who am I fighting

--- start.py
+++ start.py
@@ -1,44 +1,15 @@
-import cv2 
-from PIL import ImageGrab
+import os
+from PIL import ImageGrab, Image
 import numpy as np
-import easyocr
-import time
 
 pre = np.array(ImageGrab.grab())
 h = len(pre)
 w = len(pre[0])
 
-reader = easyocr.Reader(['en'], gpu=False)
-
-def drawing (f,r):   
-       
-    f = cv2.line(f, (round(w*0.5),0), (round(w*0.5), h), (0,255,0), 20)
-    f = cv2.line(f, (round(w*0.25),h//2), (round(w*0.75), h//2), (0,255,0), 20)
-    for r_ in r:
-        p, t, x = r_
-        f = cv2.rectangle(f, p[0], p[2], (0,255,0), 5)
-        f = cv2.putText(f, t.lower(), p[0], cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0))
-    return f
-
-count = 0 
-while True:
-
-    img = np.array(ImageGrab.grab())
-
-    if count <= 1:
-        text = reader.readtext(img)
-        img = drawing(img, text)
-        
-
-   
+i = ImageGrab.grab()
+box = (21,73,186,231)
+i =  i.crop(box)
+np.array(i)
+for f in os.listdir("character portraits"):
+    img = Image.open("character portraits/"+f)
     
-
-        imgShow = cv2.resize(img, (1000,1000))
-        cv2.imshow("cam", cv2.cvtColor(imgShow, cv2.COLOR_BGR2RGB))
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-    count += 1
-
-
-cv2.destroyAllWindows()
