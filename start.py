eb2db2fbcb5ccd8bba29e93f3ed94617c7fa208a
yousefHalsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2025-06-12 16:45:14 +0300

    testing the pixel idea

--- start.py
+++ start.py
@@ -2,14 +2,19 @@ import os
 from PIL import ImageGrab, Image
 import numpy as np
 
-pre = np.array(ImageGrab.grab())
-h = len(pre)
-w = len(pre[0])
+
+
 
 i = ImageGrab.grab()
 box = (21,73,186,231)
 i =  i.crop(box)
-np.array(i)
+i.save("crop.png")
+player_1 = np.array(i)
 for f in os.listdir("character portraits"):
-    img = Image.open("character portraits/"+f)
-    
+    img = np.array(Image.open("character portraits/"+f))
+    y_por = len(img)//2
+    x_por = len(img[0])//2
+    print(f'For {f}, on ({y_por}, {x_por}) the color is {img[y_por][x_por]}')
+
+
+
